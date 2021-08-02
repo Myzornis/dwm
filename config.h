ecc00f3e611402320dfc0141ea9b408f7b29836c
sivakumar <sivkmr2312@gmail.com>
2021-08-02 12:44:56 +0530

    added emoji fonts

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Roboto:size=12" , "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Roboto:size=12" , "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Roboto:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "🏠", 📁", "🧰", "🖥️", "⚙️"};
+static const char *tags[] = { "🏠","📁", "🧰", "🖥️", "⚙️"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -39,9 +39,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "🪟",      tile },    /* first entry is default */
+	{ "🎞️",      NULL },    /* no layout function means floating behavior */
+	{ "🖼️",      monocle },
 };
 #include "shiftview.c"
 /* key definitions */
