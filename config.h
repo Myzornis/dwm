71783778f5ac38498eb46a11c79bce89f8a5789a
sivakumar <sivkmr2312@gmail.com>
2021-08-03 12:40:52 +0530

    added pywall suppourt for dwm

--- config.h
+++ config.h
@@ -7,17 +7,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Roboto:size=12" , "JoyPixels:pixelsize=16:antialias=true:autohint=true"    };
 static const char dmenufont[]       = "Roboto:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
-
+//static const char col_gray1[]       = "#222222";
+//static const char col_gray2[]       = "#444444";
+//static const char col_gray3[]       = "#bbbbbb";
+//static const char col_gray4[]       = "#eeeeee";
+//static const char col_cyan[]        = "#005577";
+//static const char *colors[][3]      = {
+//	/*               fg         bg         border   */
+//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+//};
+#include "/home/knight/.cache/wal/colors-wal-dwm.h"
 /* tagging */
 static const char *tags[] = { "🏠","📁", "🧰", "📟", "⚙️"};
 
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb",norm_bg, "-nf",norm_fg, "-sb",sel_bg, "-sf",sel_fg, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
